a88aa9b02ecdbbf10fbf85684ae2dbc1bef2ad4a
pkujundzic <pavao.kujundzic@sedamit.hr>
2022-02-25 10:29:47 +0100

    sync

--- src/main/java/hr/sedamit/bss/databasemigrations/step/MigrationProcessor.java
+++ src/main/java/hr/sedamit/bss/databasemigrations/step/MigrationProcessor.java
@@ -1,11 +1,10 @@
 package hr.sedamit.bss.databasemigrations.step;
 
-import hr.sedamit.bss.databasemigrations.batch.repository.TableProcessConfigurationRepository;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 
 
 import java.util.Arrays;
@@ -14,7 +13,8 @@ import java.util.Map;
 
 @Component
 @StepScope
-public class MigrationProcessor implements ItemProcessor<Map<String, List<Map<String, Object>>>, Map<String, List<Map<String, Object>>>> {
+public class MigrationProcessor implements ItemProcessor
+        <Map<String, List<Map<String, Object>>>, Map<String, List<Map<String, Object>>>> {
 
     @Value("#{jobParameters['updateTables']}")
     private String updateTablesString;
@@ -22,17 +22,8 @@ public class MigrationProcessor implements ItemProcessor<Map<String, List<Map<St
     @Value("#{jobParameters['appendTables']}")
     private String appendTablesString;
 
-    @Autowired
-    private TableProcessConfigurationRepository tableProcessConfigurationRepository;
-
-
     @Override
     public Map<String, List<Map<String, Object>>> process(Map<String, List<Map<String, Object>>> data) throws Exception {
-        List<String> appendTables = Arrays.asList(appendTablesString.split("\\s*,\\s*"));
-
-
-
-
 
         return data;
     }
